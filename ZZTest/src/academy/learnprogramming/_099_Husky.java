37ec8b4a55aeaf6483713af23b8b181db7ee194c
Sanliurfa <sanliurfa@users.noreply.github.com>
2018-02-14 21:55:34 +0100

    Bluej project

---
+++ ZZTest/src/academy/learnprogramming/_099_Husky.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class _099_Husky extends _099_Dog {
+	public _099_Husky() {
+//		this();							// this() roept de constructor op van de huidige class
+//		super();						// super() roept de constructor van parent class
+		System.out.println("Husky");
+	}
+
+}
