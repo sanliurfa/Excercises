3a3d09dbfea34bb456c47e564cd7258c01c0a771
sanliurfa <sanliurfa@users.noreply.github.com>
2018-05-16 15:43:59 +0200

    20180516-001

---
+++ ZZTest/src/academy/learnprogramming/_038_QuestionsExamples.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class _038_QuestionsExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ZZTest/src/academy/learnprogramming/_045_QuestionExamples.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class _045_QuestionExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
