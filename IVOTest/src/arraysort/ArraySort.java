c0db965b7351d244b42e3a8fac4788753267de47
sanliurfa <sanliurfa@users.noreply.github.com>
2018-09-14 15:29:24 +0200

    20180914001

---
+++ IVOTest/src/arraysort/ArraySort.java
@@ -0,0 +1,18 @@
+package arraysort;
+
+import java.util.*;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		List<Integer> p = new ArrayList<>();
+		
+		p.add(7);
+		p.add(2);
+		p.add(5);
+		p.add(2);
+		p.sort(null);
+		System.out.println(p);
+	}
+
+}
--- IVOTest/src/flowcontroledowhile/DoWhile.java
+++ IVOTest/src/flowcontroledowhile/DoWhile.java
@@ -11,7 +11,7 @@ public class DoWhile {
 			while(ii < table.length) {
 				System.out.println(ii++);		// hier loopt het drie keer dus 0, 1, 2
 			}
-		} while (ii < table.length);			// omdat nu ii, drie geworden is kan het niet verder uitgevoerd zodat de uislag 0, 1, 2 blijft.
+		} while (ii < table.length);			// omdat nu ii, drie geworden is kan het niet verder uitgevoerd zodat de uitslag 0, 1, 2 blijft.
 
 		
 	}
